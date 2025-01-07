9d464b2a049607f9a805fc51db78465effdcb0fc
Shenshining <353824385@qq.com>
2025-01-07 13:42:06 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div>
+      this is a App
+      <Button type="primary">button</Button>
+    </div>
+  )
+}
+
+export default App;
